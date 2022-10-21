3377f289d641869f198a01430763d094c9536f9d
italorafaeltavares <italorafaeltavares@gmail.com>
2022-10-21 19:56:49 -0300

    Add monitoring

--- k8s.tf
+++ k8s.tf
@@ -1,7 +1,7 @@
 resource "digitalocean_kubernetes_cluster" "k8s" {
   name    = "k8s"
   region  = var.region
-  version = "1.24.4-do.1"
+  version = "1.24.4-do.0"
 
   node_pool {
     name       = "default"
---
+++ script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt full-upgrade -y
+sudo apt install -y openjdk-11-jre 
+
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y jenkins 
+
+curl -fsSL https://get.docker.com/ | bash
+usermod -aG docker jenkins
+systemctl restart jenkins
+
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+sudo apt-get install -y apt-transport-https
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
